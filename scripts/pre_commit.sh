b8180fb3ce8d6cca2a3386e04efdd450e0924b51
David Gitz <davidgitz@gmail.com>
2026-06-22 03:28:43 -0500

    WIP

--- scripts/pre_commit.sh
+++ scripts/pre_commit.sh
@@ -1,2 +1,2 @@
 java -jar /usr/bin/plantuml.jar -r "**.puml"
-find . -type f -name "*.dia" -exec bash -c 'for f; do dia --size=2000x -n -e "${f%.dia}.png" "$f"; done' _ {} +
+find . -type f -name "*.dia" -exec bash -c 'for f; do dia -t svg -n -e "${f%.dia}.svg" "$f"; done' _ {} +
