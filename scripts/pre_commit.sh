77f45e0ed4edf422791aea123534e200c68f4567
David Gitz <davidgitz@gmail.com>
2026-06-25 17:48:44 -0500

    Updates

--- scripts/pre_commit.sh
+++ scripts/pre_commit.sh
@@ -1,23 +1,27 @@
-echo "Generating Doxygen Documentation"
+echo "Generating Doxygen Documentation..."
 doxygen
 if [ $? -ne 0 ]; then
     echo "❌ Error: Doxygen documentation Checking failed. Aborting commit."
     exit 1
 fi
+echo "Ok!"
 
-echo "Converting plantuml files to images"
+echo "Converting plantuml files to images..."
 git diff --staged --name-only --diff-filter=d -- '*.puml'   | xargs -I {} java -jar /usr/bin/plantuml.jar "{}"
 if [ $? -ne 0 ]; then
     echo "❌ Error: Plantuml Generation failed. Aborting Commit."
     exit 1
 fi
+echo "Ok!"
 
-echo "Converting dia files to images"
+echo "Converting dia files to images..."
 git diff --staged --name-only --diff-filter=d -- '*.dia' | xargs -I {} dia -t svg -n -e "{}"
+echo "Ok!"
 
-echo "Checking Markdown Links"
+echo "Checking Markdown Links..."
 markdown-link-check . -i build/ -q
 if [ $? -ne 0 ]; then
     echo "❌ Error: Markdown Link Checking failed. Aborting commit."
     exit 1
 fi
+echo "Ok!"
