f765173f7727bb308eb681efeb729e386d97ce0d
David Gitz <davidgitz@gmail.com>
2026-06-26 15:59:12 -0500

    Adding CI.

---
+++ scripts/setup_ide.sh
@@ -0,0 +1,11 @@
+echo "Setting up IDE"
+sudo apt update
+pip install --upgrade pip
+pip install pre-commit
+pipx install cmake-format
+pushd ~/Downloads
+wget https://github.com/plantuml/plantuml/releases/download/v1.2026.6/plantuml-1.2026.6.jar
+sudo mv plantuml-*.jar /usr/bin/plantuml.jar
+popd
+sudo apt install python3-cookiecutter
+sudo npm install -g markdown-link-check
