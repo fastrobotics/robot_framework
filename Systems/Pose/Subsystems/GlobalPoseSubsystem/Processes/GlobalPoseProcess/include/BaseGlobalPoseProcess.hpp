083e5825b1180661633f358c51e83287323bfb0f
David Gitz <davidgitz@gmail.com>
2026-06-27 04:41:03 -0500

    WIP

--- Systems/Pose/Subsystems/GlobalPoseSubsystem/Processes/GlobalPoseProcess/include/BaseGlobalPoseProcess.hpp
+++ Systems/Pose/Subsystems/GlobalPoseSubsystem/Processes/GlobalPoseProcess/include/BaseGlobalPoseProcess.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file BaseGlobalPoseProcess.hpp
+ * @author yDavid Gitz (davidgitz@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-06-27
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
 #pragma once
 #include <IGlobalPoseProcess.hpp>
 namespace fast::rf::PoseSystem::GlobalPoseSubsystem {
