ae64ebd22fc820d46be143caac15423365bec9d0
David Gitz <davidgitz@gmail.com>
2026-06-27 15:36:14 -0500

    Working on message definition.

--- Systems/Pose/Subsystems/GlobalPoseSubsystem/Processes/GlobalPoseProcess/include/BaseGlobalPoseProcess.hpp
+++ Systems/Pose/Subsystems/GlobalPoseSubsystem/Processes/GlobalPoseProcess/include/BaseGlobalPoseProcess.hpp
@@ -32,9 +32,22 @@ public:
    * @return uint8_t The number of supported gps channels
    */
   uint8_t get_supported_gps_channel_count() { return supported_gps_channels; }
+  /**
+   * @brief Get the GlobalPose object
+   *
+   * @return GeometryMsgs::OdomMsg
+   */
   GeometryMsgs::OdomMsg get_GlobalPose() override {
     return global_pose;
   } //!< Get Global Pose
+  /**
+   * @brief Update the base object
+   *
+   * @param current_time_sec
+   * @param delta_time_sec
+   * @return true If ok
+   * @return false If not ok
+   */
   bool base_update(double current_time_sec,
                    double delta_time_sec); //!< Base function to update
 
