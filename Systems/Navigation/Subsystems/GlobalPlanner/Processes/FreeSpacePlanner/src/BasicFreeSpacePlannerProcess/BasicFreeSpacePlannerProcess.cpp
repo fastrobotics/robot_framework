f0158bccf1d2a30d33e468e4c54441f6436a245e
David Gitz <davidgitz@gmail.com>
2026-07-04 05:40:46 -0500

    WIP

--- Systems/Navigation/Subsystems/GlobalPlanner/Processes/FreeSpacePlanner/src/BasicFreeSpacePlannerProcess/BasicFreeSpacePlannerProcess.cpp
+++ Systems/Navigation/Subsystems/GlobalPlanner/Processes/FreeSpacePlanner/src/BasicFreeSpacePlannerProcess/BasicFreeSpacePlannerProcess.cpp
@@ -1,11 +1,18 @@
 #include <BasicFreeSpacePlannerProcess/BasicFreeSpacePlannerProcess.hpp>
 namespace fast::rf::NavigationSystem::GlobalPlannerSubsystem {
-bool BasicFreeSpacePlannerProcess::update(double current_time_sec, [[maybe_unused]] double delta_time_sec) {
-    bool status = BaseFreeSpacePlannerProcess::base_update(current_time_sec, delta_time_sec);
-    if (status == false) {
-        return false;
+    bool BasicFreeSpacePlannerProcess::init() {
+        std::vector<fast::rf::DiagnosticDefinition::DiagnosticType> diagnostic_types;
+        diagnostic_types.push_back(fast::rf::DiagnosticDefinition::DiagnosticType::SOFTWARE);
+        // Add others as Needed
+        bool status = diagnosticManager.initialize_diagnostics(diagnostic_types);
+        return status;
     }
-    return true;
-}
+    bool BasicFreeSpacePlannerProcess::update(double current_time_sec, [[maybe_unused]] double delta_time_sec) {
+        bool status = BaseFreeSpacePlannerProcess::base_update(current_time_sec, delta_time_sec);
+        if (status == false) {
+            return false;
+        }
+        return true;
+    }
 
 }  // namespace fast::rf::NavigationSystem::GlobalPlannerSubsystem
