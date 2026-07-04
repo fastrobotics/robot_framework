7750aa5db72a920861ccd4a88126f63e1ebd7787
David Gitz <davidgitz@gmail.com>
2026-07-04 10:26:22 -0500

    WIP

--- Systems/Navigation/Subsystems/GlobalPlanner/Processes/GlobalPlannerManager/src/BasicGlobalPlannerManagerProcess/test/test_BasicGlobalPlannerManagerProcess.cpp
+++ Systems/Navigation/Subsystems/GlobalPlanner/Processes/GlobalPlannerManager/src/BasicGlobalPlannerManagerProcess/test/test_BasicGlobalPlannerManagerProcess.cpp
@@ -9,6 +9,7 @@ using namespace fast::rf::NavigationSystem::GlobalPlannerSubsystem;
 
 TEST(BasicGlobalPlannerManagerProcess, BasicTests) {
     BasicGlobalPlannerManagerProcess SUT;
+    ASSERT_TRUE(SUT.init());
     ASSERT_TRUE(SUT.update(0.0, 0.0));
 }
 TEST(BasicGlobalPlannerManagerProcess, BasicConversionTests) {
