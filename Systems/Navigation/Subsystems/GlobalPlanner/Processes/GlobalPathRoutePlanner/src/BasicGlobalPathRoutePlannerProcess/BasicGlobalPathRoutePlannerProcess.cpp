4a2d572606c47386456b4bb2438b48a539c40a46
David Gitz <davidgitz@gmail.com>
2026-07-04 05:52:18 -0500

    WIP

--- Systems/Navigation/Subsystems/GlobalPlanner/Processes/GlobalPathRoutePlanner/src/BasicGlobalPathRoutePlannerProcess/BasicGlobalPathRoutePlannerProcess.cpp
+++ Systems/Navigation/Subsystems/GlobalPlanner/Processes/GlobalPathRoutePlanner/src/BasicGlobalPathRoutePlannerProcess/BasicGlobalPathRoutePlannerProcess.cpp
@@ -1,11 +1,18 @@
 #include <BasicGlobalPathRoutePlannerProcess/BasicGlobalPathRoutePlannerProcess.hpp>
 namespace fast::rf::NavigationSystem::GlobalPlannerSubsystem {
-bool BasicGlobalPathRoutePlannerProcess::update(double current_time_sec, [[maybe_unused]] double delta_time_sec) {
-    bool status = BaseGlobalPathRoutePlannerProcess::base_update(current_time_sec, delta_time_sec);
-    if (status == false) {
-        return false;
+    bool BasicGlobalPathRoutePlannerProcess::init() {
+        std::vector<fast::rf::DiagnosticDefinition::DiagnosticType> diagnostic_types;
+        diagnostic_types.push_back(fast::rf::DiagnosticDefinition::DiagnosticType::SOFTWARE);
+        // Add more as needed
+        bool status = diagnosticManager.initialize_diagnostics(diagnostic_types);
+        return status;
     }
-    return true;
-}
+    bool BasicGlobalPathRoutePlannerProcess::update(double current_time_sec, [[maybe_unused]] double delta_time_sec) {
+        bool status = BaseGlobalPathRoutePlannerProcess::base_update(current_time_sec, delta_time_sec);
+        if (status == false) {
+            return false;
+        }
+        return true;
+    }
 
 }  // namespace fast::rf::NavigationSystem::GlobalPlannerSubsystem
