5dac31ad2f81ec32239a384843482718be033fa6
David Gitz <davidgitz@gmail.com>
2026-07-19 09:27:19 -0500

    Updating Tank Drive Executor Process.

--- Systems/Navigation/Subsystems/NavigationExecutor/Processes/DriveExecutor/src/BaseDriveExecutorProcess.cpp
+++ Systems/Navigation/Subsystems/NavigationExecutor/Processes/DriveExecutor/src/BaseDriveExecutorProcess.cpp
@@ -1,14 +1,18 @@
 #include <BaseDriveExecutorProcess.hpp>
 namespace fast::rf::NavigationSystem::NavigationExecutorSubsystem {
-    bool BaseDriveExecutorProcess::base_update(double current_time_sec) {
+    bool BaseDriveExecutorProcess::update(double current_time_sec) {
         current_time_sec_ = current_time_sec;
-
+        if (diagnosticManager.get_diagnostics(fast::rf::Level::ERROR).size() == 0) {
+            ready_to_arm.ready_to_arm = true;
+        } else {
+            ready_to_arm.ready_to_arm = false;
+        }
         return true;
     }
     std::string BaseDriveExecutorProcess::pretty() {
         std::string str = "\n---Drive Executor---\n";
         str += "\tT: " + std::to_string(current_time_sec_) + "\n";
-
+        str += "\tReady To Arm: " + std::to_string(ready_to_arm.ready_to_arm) + "\n";
         str += diagnosticManager.pretty();
         return str;
     }
