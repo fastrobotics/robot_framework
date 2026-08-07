5b43885dc6bf226b7ef92514e36bf3dd0429bbcc
David Gitz <davidgitz@gmail.com>
2026-08-06 21:21:43 -0500

    Added support to apply joystick calibration.

--- Systems/UserInterface/Subsystems/RemoteControl/Processes/TeleopControl/include/JoystickScaler.hpp
+++ Systems/UserInterface/Subsystems/RemoteControl/Processes/TeleopControl/include/JoystickScaler.hpp
@@ -18,7 +18,7 @@ namespace fast::rf::UserInterfaceSystem::RemoteControlSubsystem {
      */
     class JoystickScaler {
        public:
-        static constexpr double AXIS_MAX_VALUE = 1.0;  //!< The Max value of the Axis
+        static constexpr double AXIS_MAX_VALUE = 100.0;  //!< The Max value of the Axis
         /**
          * @brief Initialize the object
          *
