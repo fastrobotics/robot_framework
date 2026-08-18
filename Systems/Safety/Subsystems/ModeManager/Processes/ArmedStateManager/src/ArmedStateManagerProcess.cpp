ccfdd931572ef452e0ca272fa0f3da3a7c9292f6
David Gitz <davidgitz@gmail.com>
2026-08-17 20:49:35 -0500

    Updates

--- Systems/Safety/Subsystems/ModeManager/Processes/ArmedStateManager/src/ArmedStateManagerProcess.cpp
+++ Systems/Safety/Subsystems/ModeManager/Processes/ArmedStateManager/src/ArmedStateManagerProcess.cpp
@@ -46,6 +46,10 @@ namespace fast::rf::SafetySystem::ModeManagerSubsystem {
             fast::rf::NavigationSystem::NavigationExecutorSubsystem::SUBSYSTEM_ID,
             fast::rf::NavigationSystem::NavigationExecutorSubsystem::PROCESS_TRAJECTORY_CONTROLLER_ID);
 
+        ready_to_arm_computer.add_monitor(fast::rf::PoseSystem::SYSTEM_ID,
+                                          fast::rf::PoseSystem::LocalPoseSubsystem::SUBSYSTEM_ID,
+                                          fast::rf::PoseSystem::LocalPoseSubsystem::PROCESS_LOCALPOSEFUSER_ID);
+
         // GCOV_EXCL_START
         status = ready_to_arm_computer.init();
         if (status == false) {
