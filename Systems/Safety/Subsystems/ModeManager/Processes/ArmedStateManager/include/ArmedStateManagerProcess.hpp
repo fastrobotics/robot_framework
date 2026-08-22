afeab12ce5f589cd413625b87aaecd9e23f2d6ad
David Gitz <davidgitz@gmail.com>
2026-08-22 07:20:35 -0500

    Major work complete.

--- Systems/Safety/Subsystems/ModeManager/Processes/ArmedStateManager/include/ArmedStateManagerProcess.hpp
+++ Systems/Safety/Subsystems/ModeManager/Processes/ArmedStateManager/include/ArmedStateManagerProcess.hpp
@@ -14,7 +14,7 @@
 #include <BaseArmedStateManagerProcess.hpp>
 #include <ReadyToArmComputer.hpp>
 namespace fast::rf::SafetySystem::ModeManagerSubsystem::ArmedStateManager {
-    struct Test {};
+    struct Test b {};
     /**
      * @brief Minimal Implementation for a ArmedStateManager Process
      *
