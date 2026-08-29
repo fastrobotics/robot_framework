9c6ff6e8f2bee4fa416bfa7fe602c45e2f5a6d16
David Gitz <davidgitz@gmail.com>
2026-08-29 08:57:26 -0500

    WIP

--- scripts/pre_commit/run_clangtidy.sh
+++ scripts/pre_commit/run_clangtidy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Running Clang-Tidy Code..."
-git diff -U0 --staged -- '*.cpp' '*.hpp' | grep -vE '^(b/)?(templates/|build/)' | clang-tidy-diff -p1 -- -std=c++17
+git diff -U0 --staged -- '*.cpp' '*.hpp' ':(exclude)templates' ':(exclude)build' | clang-tidy-diff -p1 -path build/
+
