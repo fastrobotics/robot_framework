b2b84ca9b212050fa8ed2a8a8bb22cf4cb6b7709
David Gitz <davidgitz@gmail.com>
2026-09-22 21:11:55 -0500

    Updates

--- scripts/pre_commit/mermaid_conversion.sh
+++ scripts/pre_commit/mermaid_conversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Converting mermaid files to images..."
-git diff --staged --name-only --diff-filter=d -- '*.mmd' | xargs -I {} bash -c 'mmdc -i "$1" -o "${1%.mmd}.png"' _ {}
+git diff --staged --name-only --diff-filter=d -- '*.mmd' | xargs -I {} bash -c 'mmdc -i "$1" -o "${1%.mmd}.png" --scale 2' _ {}
 echo "Ok!"
