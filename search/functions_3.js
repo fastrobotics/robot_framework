48c0e5983232328c10a0a1d9acc1b02ffb88ad9e
dgitz <davidgitz@gmail.com>
2026-09-23 00:15:27 +0000

    Deploying to gh-pages from  @ 10a054256d265f1a5ce70864eb8f8c532fd7455e 🚀

--- search/functions_3.js
+++ search/functions_3.js
@@ -3,5 +3,6 @@ var searchData=
   ['datapacket_0',['DataPacket',['../structfast_1_1rf_1_1PoseSystem_1_1InertialSensorSubsystem_1_1IMU_1_1BaseIMUDriver_1_1DataPacket.html#a56319241cf8b9f8c7e9e98efeec0d7ea',1,'fast::rf::PoseSystem::InertialSensorSubsystem::IMU::BaseIMUDriver::DataPacket']]],
   ['diagnosticmanager_1',['DiagnosticManager',['../classfast_1_1rf_1_1core_1_1infrastructure_1_1DiagnosticManager.html#ac49b9e668502269c10a121b3cbcf958f',1,'fast::rf::core::infrastructure::DiagnosticManager']]],
   ['diagnosticmsg_2',['diagnosticmsg',['../structfast_1_1rf_1_1messages_1_1InfrastructureMsgs_1_1DiagnosticMsg.html#a7ff6163b235bc2c31661ac99a1d89958',1,'fast::rf::messages::InfrastructureMsgs::DiagnosticMsg::DiagnosticMsg()'],['../structfast_1_1rf_1_1messages_1_1InfrastructureMsgs_1_1DiagnosticMsg.html#ad790d00d4542a2743e4fd39c0975b02b',1,'fast::rf::messages::InfrastructureMsgs::DiagnosticMsg::DiagnosticMsg(uint8_t systemID, uint8_t subsystemID, uint8_t processID, fast::rf::DiagnosticDefinition::DiagnosticType diagnosticType)']]],
-  ['differentiate_3',['differentiate',['../classfast_1_1rf_1_1PoseSystem_1_1PoseUtility.html#a654f509f8dd157d5b4de0d06bce510cd',1,'fast::rf::PoseSystem::PoseUtility']]]
+  ['differentiate_3',['differentiate',['../classfast_1_1rf_1_1PoseSystem_1_1PoseUtility.html#a654f509f8dd157d5b4de0d06bce510cd',1,'fast::rf::PoseSystem::PoseUtility']]],
+  ['disableconsoleprint_4',['disableConsolePrint',['../classfast_1_1rf_1_1Logger.html#a10a7c6b00305c0e114281234059f901f',1,'fast::rf::Logger']]]
 ];
